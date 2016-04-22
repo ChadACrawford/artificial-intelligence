bf51e35efecfc89acb88a92b7fae5cf508531866
Chad Crawford <chad-crawford@utulsa.edu>
2016-04-22 16:12:05 -0500

    Fixing continuous state learner.

--- mdp/cnav.py
+++ mdp/cnav.py
@@ -3,6 +3,7 @@ import math
 import numpy.random as npr
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 
 
 def distance(x1, x2):
@@ -31,7 +32,7 @@ class ContinuousNavigation(mdp.ContinuousStateMDP):
         return False
 
     def start(self):
-        return np.tile(0., self.num_dimensions)
+        return np.tile(1.0, self.num_dimensions)
 
     def add_rock(self, s, r):
         self._rocks[s] = r
@@ -60,23 +61,24 @@ class ContinuousNavigation(mdp.ContinuousStateMDP):
             return g
         return self._step_cost
 
-    move_left = np.array([.5, 0.], dtype='float64')
-    move_right = np.array([-.5, 0.], dtype='float64')
+    move_left = np.array([-.5, 0.], dtype='float64')
+    move_right = np.array([.5, 0.], dtype='float64')
     move_up = np.array([0., .5], dtype='float64')
     move_down = np.array([0., -.5], dtype='float64')
 
     def move(self, s, a):
         s = np.copy(s)
         if a == "up":
-            s += self.move_up + npr.multivariate_normal(np.array([0., 0]), np.diag([0.25,0.125]))
+            s = s + self.move_up + npr.multivariate_normal(np.array([0., 0]), np.diag([0.25, 0.125]))
         elif a == "down":
-            s += self.move_down + npr.multivariate_normal(np.array([0., 0]), np.diag([0.25, 0.125]))
+            s = s + self.move_down + npr.multivariate_normal(np.array([0., 0]), np.diag([0.25, 0.125]))
         elif a == "left":
-            s += self.move_left + npr.multivariate_normal(np.array([0., 0]), np.diag([0.125, 0.25]))
+            s = s + self.move_left + npr.multivariate_normal(np.array([0., 0]), np.diag([0.125, 0.25]))
         elif a == "right":
-            s += self.move_right + npr.multivariate_normal(np.array([0., 0]), np.diag([0.125, 0.25]))
+            s = s + self.move_right + npr.multivariate_normal(np.array([0., 0]), np.diag([0.125, 0.25]))
         else:
             pass
+        # print s
         return s
 
     def act(self, s, a):
@@ -102,12 +104,25 @@ import linear_fapprox as lf
 def mdp1():
     m = ContinuousNavigation(10, 10, -0.01)
     m.add_goal((10, 10), 2, 10)
-    m.add_goal((10, 1), 1, -10)
+    m.add_goal((10, 1), 6, -10)
     m.add_rock((5, 5), 2)
     return m, 0, 10, 0, 10
 
 
-def gen_heatmap(theta, f, xmin, xmax, ymin, ymax, dx, dy):
+def gen_heatmap(f_exp, f, xmin, xmax, ymin, ymax, dx, dy):
+    nx = int(math.ceil((xmax-xmin)/dx))
+    ny = int(math.ceil((ymax-ymin)/dy))
+    q = np.zeros((ny, nx))
+    for xi in range(nx):
+        x = xmin + xi * dx
+        for yi in range(ny):
+            y = ymin + yi * dy
+            #phi = f.features((x, y))
+            q[yi, xi] = f_exp((x, y))
+    return q
+
+
+def gen_heatmap_2(f, xmin, xmax, ymin, ymax, dx, dy):
     nx = int(math.ceil((xmax-xmin)/dx))
     ny = int(math.ceil((ymax-ymin)/dy))
     q = np.zeros((nx, ny))
@@ -116,30 +131,53 @@ def gen_heatmap(theta, f, xmin, xmax, ymin, ymax, dx, dy):
         for yi in range(ny):
             y = ymin + yi * dy
             phi = f.features((x, y))
-            q[xi, yi] = sum(theta * phi)
+            q[xi, ny-yi-1] = sum(np.arange(0, f.num_features) * phi)
     return q
+
+
+import pprint
 
 
 def run():
     m, xmin, xmax, ymin, ymax = mdp1()
     fig = plt.gcf()
-    f = lf.TileEncoding(m, 5, 1, 10)
-    pi, theta = lf.qlearn1(m, f, num_episodes=100)
-    heatmap = gen_heatmap(theta, f, xmin, xmax, ymin, ymax, 0.1, 0.1)
-    print heatmap
+    # f = lf.TileEncoding(m, 5, 1, 14)
+    f = lf.RadialBasis(m, 200, 2)
+    pi, f_exp, theta = lf.qlearn2(m, f, num_episodes=500)
+    # pp = pprint.PrettyPrinter()
+    # pp.pprint(theta)
+    heatmap = gen_heatmap(f_exp, f, xmin, xmax, ymin, ymax, 0.1, 0.1)
+    # heatmap = gen_heatmap_2(f, xmin, xmax, ymin, ymax, 0.05, 0.05)
+    # print heatmap
     plt.clf()
-    plt.imshow(heatmap)
+    cax = plt.imshow(heatmap, origin="lower")
+    hmin, hmax = np.min(heatmap), np.max(heatmap)
+    cbar = plt.colorbar(cax, ticks=[hmin, 0, hmax])
     # m.draw(fig)
-    # x = npr.uniform(m.dimensions[0][0], m.dimensions[0][1], 1000)
-    # y = npr.uniform(m.dimensions[1][0], m.dimensions[1][1], 1000)
-    # to_v = lambda x: (-1, 0) if x == "left" else\
-    #     (1, 0) if x == "right" else\
-    #     (0, 1) if x == "up" else\
-    #     (0, -1)
-    # vx, vy = zip(*[to_v(pi((x1, y1))) for x1, y1 in zip(x, y)])
-    # plt.quiver(x, y, vx, vy)
+    x = np.linspace(m.dimensions[0][0], m.dimensions[0][1], 20)
+    y = np.linspace(m.dimensions[1][0], m.dimensions[1][1], 20)
+    x, y = zip(*itertools.product(list(x), list(y)))
+    to_v = lambda x: (-1, 0) if x == "left" else\
+        (1, 0) if x == "right" else\
+        (0, 1) if x == "up" else\
+        (0, -1)
+    vx, vy = zip(*[to_v(pi((x1, y1))) for x1, y1 in zip(x, y)])
+    x = [10*x_i for x_i in x]
+    y = [10*y_i for y_i in y]
+    plt.quiver(x, y, vx, vy)
     plt.show()
+    plt.savefig("radial_basis.pdf", format="pdf")
 
+
+def test():
+    m, xmin, xmax, ymin, ymax = mdp1()
+    s = m.start()
+    print s
+    for _ in range(100):
+        a = npr.choice(m.actions(s))
+        s2, r2 = m.act(s, a)
+        print a, s2, np.sqrt(np.sum((s2 - s)**2))
+        s = s2
 
 if __name__ == '__main__':
     run()
--- mdp/linear_fapprox.py
+++ mdp/linear_fapprox.py
@@ -1,9 +1,10 @@
 import numpy as np
 import numpy.random as npr
 import math
+import random
 
 
-def qlearn1(m, f, lmbda=0.8, alpha=0.1, gamma=0.999, epsilon=0.2, num_episodes=100):
+def qlearn1(m, f, lmbda=0.1, alpha=0.3, gamma=0.9999, epsilon=0.2, num_episodes=100):
     theta = np.zeros((f.num_features,))
     t = 1
     for _ in range(num_episodes):
@@ -23,7 +24,7 @@ def qlearn1(m, f, lmbda=0.8, alpha=0.1, gamma=0.999, epsilon=0.2, num_episodes=1
                 phi_a2 = feature_estimate(m, s2, a2, f)
                 q.append(sum(theta * phi_a2))
             delta += (gamma**t) * max(q)
-            theta += alpha * delta * e
+            theta = (1-alpha) * theta + alpha * delta * e
             if npr.random() < 1 - epsilon:
                 a2 = max(zip(m.actions(s2), q), key=lambda x: x[1])[0]
                 e *= (gamma**t) * lmbda
@@ -36,6 +37,45 @@ def qlearn1(m, f, lmbda=0.8, alpha=0.1, gamma=0.999, epsilon=0.2, num_episodes=1
 
     pi = lambda s: max([(a, sum(theta * feature_estimate(m, s, a, f))) for a in m.actions(s)], key=lambda x: x[1])[0]
     return pi, theta
+
+
+def qlearn2(m, f, alpha=0.5, gamma=1.0, epsilon=0.2, num_episodes=1000):
+    actions = m.actions()
+    theta = dict()
+    for a in actions:
+        theta[a] = np.zeros((f.num_features,))
+    for episode in range(num_episodes):
+        t = 0
+        s = m.start()
+        a = npr.choice(m.actions(s))
+        q = 0
+        while not m.is_terminal(s):
+            s2, r2 = m.act(s, a)
+            # if r2 < -1 or s2[0] > 5 and s2[1] < 5:
+            #     print "wat", s2, r2
+            qp = sum(theta[a] * f.features(s))
+            # print a, s, s2, r2, [(a, r2 + gamma * qp) for a in m.actions(s)]
+            actions = m.actions(s)
+            random.shuffle(actions)
+            a2, q2 = max([(a2, r2 + gamma * sum(theta[a2] * f.features(s2))) for a2 in actions], key=lambda x: x[1])
+            delta = q2 - qp
+            # if random.random() < 0.01:
+            #     print delta
+            # print a, delta, s, s2
+            theta[a] += alpha * delta * (f.features(s))
+            if npr.random() < epsilon:
+                a2 = npr.choice(m.actions(s))
+            q = q2
+            s = s2
+            a = a2
+            t += 1
+        print episode+1, t, s, r2
+
+
+    pi = lambda s: max([(a, sum(theta[a] * f.features(s))) for a in m.actions(s)], key=lambda x: x[1])[0]
+    f_exp = lambda s: max(sum(theta[a] * f.features(s)) for a in m.actions(s))
+    return pi, f_exp, theta
+
 
 
 def feature_estimate(m, s, a, f):
@@ -71,7 +111,7 @@ class TileEncoding(LinearApprox):
 
     def __construct_features(self, m):
         dims = m.dimensions
-        self._widths = [(x2 - x1) / self._num_tiles for x1, x2 in dims]
+        self._widths = [(1. * x2 - x1) / (self._num_tiles-1) for x1, x2 in dims]
         self._offsets = np.zeros((self._num_layers, self._num_dimensions))
         for i in range(self._num_layers):
             for j in range(self._num_dimensions):
@@ -87,24 +127,37 @@ class TileEncoding(LinearApprox):
 
     def get_feature(self, s, d):
         offsets = self._offsets[d]
-        smap = tuple([math.floor((x + o) / w) % self._num_tiles for x, o, w in zip(s, offsets, self._widths)])
+        smap = tuple([max(min(math.floor((x + o) / w), self._num_tiles-1), 0)
+                      for x, o, w in zip(s, offsets, self._widths)])
         # print smap, offsets, self._widths
         return (d,) + smap
 
     def features(self, s):
         phi = self._empty_feature_vector
-        for d in range(self._num_dimensions):
+        for d in range(self._num_layers):
             k = self.get_feature(s, d)
+            # print s, k
             phi[k] = 1
-        return phi.flatten()
+        return phi.flatten() / np.sum(phi)
 
 
 class RadialBasis(LinearApprox):
-    def __init__(self, num_centers):
-        pass
+    def __init__(self, m, num_centers, radius):
+        self._num_centers = num_centers
+        self._radius = radius
+        self.__construct_features(m)
 
+    def __construct_features(self, m):
+        self._u = [np.array([npr.uniform(d[0], d[1]) for d in m.dimensions]) for _ in range(self._num_centers)]
+        self._r = npr.uniform(0, self._radius, self._num_centers)
+
+    @property
     def num_features(self):
-        raise NotImplementedError
+        return self._num_centers
 
     def features(self, s):
-        pass
+        phi = np.zeros((self.num_features,))
+        for k in range(len(phi)):
+            phi[k] = np.exp(- 0.5 * sum((self._u[k] - s)**2) / self._r[k])
+        # print phi
+        return phi / sum(phi)
